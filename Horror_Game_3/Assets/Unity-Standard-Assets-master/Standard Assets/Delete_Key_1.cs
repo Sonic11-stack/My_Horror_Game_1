506a4c46994d7d54461eb7b38e5ec98d0c2e0ad0
Матвей <konnorrk850@gmail.com>
2023-11-02 17:17:52 +0300

    Commit
    First commit

---
+++ Horror_Game_3/Assets/Unity-Standard-Assets-master/Standard Assets/Delete_Key_1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete_Key_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //GameObject key_1 = new GameObject();
+    void Start()
+    {
+        //key_1 = GameObject.FindWithTag("key_1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Проверяем, что объект, с которым произошло столкновение, является игроком
+            // Если да, то скрываем этот объект
+            gameObject.SetActive(false);
+        }
+    }
+}
